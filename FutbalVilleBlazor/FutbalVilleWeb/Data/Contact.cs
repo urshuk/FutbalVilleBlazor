d54bd338cea2b4d9ab4f0724e286076771709693
Vít Vašák <vitvasak@outlook.com>
2020-03-24 16:43:31 +0100

    new project with identity + sql azure server

---
+++ FutbalVilleBlazor/FutbalVilleWeb/Data/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutbalVilleWeb.Data
+{
+	public class Contact
+	{
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		public string Subject { get; set; }
+		[Required]
+		public string Message { get; set; }
+		[Phone]
+		public string Phone { get; set; }
+	}
+}
---
+++ FutbalVilleBlazor/FutbalVilleWeb/Startup.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using FutbalVilleWeb.Areas.Identity;
+using FutbalVilleWeb.Data;
+using System.Net.Http;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Options;
+
+namespace FutbalVilleWeb
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//mongoDB
+			services.Configure<LoggingDatabaseSettings>(Configuration.GetSection(nameof(LoggingDatabaseSettings)));
+			services.AddSingleton<ILoggingDatabaseSettings>(sp => sp.GetRequiredService<IOptions<LoggingDatabaseSettings>>().Value);
+
+			services.AddResponseCompression(opts =>
+			{
+				opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+					new[] { "application/octet-stream" });
+			});
+
+			// Server Side Blazor doesn't register HttpClient by default
+			if (!services.Any(x => x.ServiceType == typeof(HttpClient)))
+			{
+				// Setup HttpClient for server side in a client side compatible fashion
+				services.AddScoped<HttpClient>(s =>
+				{
+					// Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.      
+					var uriHelper = s.GetRequiredService<NavigationManager>();
+					return new HttpClient
+					{
+						BaseAddress = new Uri(uriHelper.BaseUri)
+					};
+				});
+			}
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(
+					Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+			services.AddRazorPages();
+			services.AddServerSideBlazor();
+			services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+			services.AddSingleton<WeatherForecastService>();
+			services.AddSingleton<ErrorLogService>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapBlazorHub();
+				endpoints.MapFallbackToPage("/_Host");
+			});
+		}
+	}
+}
