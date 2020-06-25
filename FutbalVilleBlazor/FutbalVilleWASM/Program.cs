a2133a36d6e207052d3bc448d16d845f8c6ad564
Vít Vašák <vitvasak@outlook.com>
2020-06-25 11:35:11 +0200

    email + api +login working

--- FutbalVilleBlazor/FutbalVilleWASM/Program.cs
+++ FutbalVilleBlazor/FutbalVilleWASM/Program.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Http;
 using BlazorStrap;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace FutbalVilleWASM
 {
@@ -20,11 +22,20 @@ namespace FutbalVilleWASM
 
 			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+			builder.Services.AddHttpClient("api").AddHttpMessageHandler(sp =>
+			{
+				var handler = sp.GetService<AuthorizationMessageHandler>()
+				.ConfigureHandler(
+						authorizedUrls: new[] { "https://localhost:44341/", "https://api.futbalville.sk/" },
+						scopes: new[] { "vasakapi" });
+				return handler;
+			});
+
 			builder.Services.AddOidcAuthentication(options =>
 			{
 				// Configure your authentication provider options here.
 				// For more information, see https://aka.ms/blazor-standalone-auth
-				//builder.Configuration.Bind("Local", options.ProviderOptions);
+				builder.Configuration.Bind("Oidc", options.ProviderOptions);
 			});
 
 			builder.Services.AddBootstrapCss();
