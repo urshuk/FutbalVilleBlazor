44f8d3b8890f85f3c707f1eac969db1b6652f475
Vít Vašák <vitvasak@outlook.com>
2020-06-30 18:28:48 +0200

    optimizing images

---
+++ FutbalVilleBlazor/FutbalVilleWeb/wwwroot/js/app.js
@@ -0,0 +1,9 @@
+﻿//carousel
+window.initializeCarousel = () => {
+    $('.carousel').carousel();
+    $('.carousel-control-prev').click(
+        () => $('.carousel').carousel('prev'));
+    $('.carousel-control-next').click(
+        () => $('.carousel').carousel('next'));
+
+}
